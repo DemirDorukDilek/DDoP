64e7a285cf6b6f216db3d2a197019051adb10e36
DemirDorukDilek <demirdorukd@gmail.com>
2026-03-12 13:28:35 +0000

    ehem

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from DDOP import optimal_traj,map_reader
+import numpy as np
 
-map_path = "maps/kan.json"
+map_path = "maps/kan2.json"
 start,goal,obstacles, bounds = map_reader(map_path)
 optimal_traj(start,goal,obstacles,bounds,plot=True,interactive=False,rrt_args={"max_iter":1_000_000})
 
