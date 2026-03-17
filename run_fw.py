c21afbe75ed070fac7de9b4dd0c1b475455a934d
DemirDorukDilek <demirdorukd@gmail.com>
2026-03-17 13:32:56 +0000

    scale

--- run_fw.py
+++ run_fw.py
@@ -1,14 +1,14 @@
 from DDOP import optimal_traj_fw, map_reader
 
-map_path = "maps/2d_init_map.json"
+map_path = "maps/2d_init_fw_map.json"
 start, goal, obstacles, bounds = map_reader(map_path)
 optimal_traj_fw(
     start, goal, obstacles, bounds,
     plot=True, interactive=True,
-    rrt_args={"max_iter": 1_000_000, "step_size": 0.5},
+    rrt_args={"max_iter": 1_000_000, "step_size": 10.0},
     optimizer_args={
-        "v_min": 10.0,
-        "v_max": 30.0,
+        "v_min": 0.0,
+        "v_max": 50.0,
         "a_max": 20.0,
         "phi_max_deg": 35.0,
         "gamma_max_deg": 20.0,
